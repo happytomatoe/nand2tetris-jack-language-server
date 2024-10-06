002ad11ce6fad118ad2903e8728c7cbb166ad097
Roman Lukash <2893931+happytomatoe@users.noreply.github.com>
2024-10-06 08:42:17 +0200

    WIP

--- prettier-plugin-jack/src/jack.visitor.ts
+++ prettier-plugin-jack/src/jack.visitor.ts
@@ -1,5 +1,10 @@
+import { BinaryOperatorContext, ClassDeclarationContext, ProgramContext } from 'jack-compiler/out/generated/JackParser';
 import JackParserVisitor from 'jack-compiler/out/generated/JackParserVisitor';
+import { Doc } from 'prettier';
+import { builders } from 'prettier/doc';
 
-export class JackVisitor extends JackParserVisitor<string> {
-
+export class JackVisitor extends JackParserVisitor<Doc[]> {
+	visitClassDeclaration = (ctx: ClassDeclarationContext): builders.Doc[] => {
+		return ["a"];
+	};
 }
--- prettier-plugin-jack/src/printer.ts
+++ prettier-plugin-jack/src/printer.ts
@@ -1,11 +1,16 @@
+import { ProgramContext } from "jack-compiler";
 import { AstPath, Doc, doc, ParserOptions } from "prettier";
+// import { JackVisitor } from "./jack.visitor";
 
-const { join, line, ifBreak, group } = doc.builders;
+// const { join, line, ifBreak, group } = doc.builders;
 export function print<T>(
 	path: AstPath<T>,
 	options: ParserOptions<T>,
 	print: (path: AstPath<T>) => Doc,
 	args?: unknown,
 ): Doc {
-	return "";
+	console.log("Inside printer");
+	// const tree = path.node as ProgramContext;
+	// return tree.accept(new JackVisitor());
+	return "a";
 }
