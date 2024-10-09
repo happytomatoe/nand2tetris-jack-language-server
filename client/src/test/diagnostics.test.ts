9bdfa9bd2adf1c94131cbe0b3d0ed26af71207bb
Roman Lukash <2893931+happytomatoe@users.noreply.github.com>
2024-10-09 19:29:47 +0200

    Add e2e tests for compiler errors

--- client/src/test/diagnostics.test.ts
+++ client/src/test/diagnostics.test.ts
@@ -1,35 +1,67 @@
-import * as vscode from 'vscode';
-import * as assert from 'assert';
-import { getDocUri, activate } from './helper';
+import * as vscode from "vscode";
+import * as assert from "assert";
+import { getDocUri, activate } from "./helper";
 
-suite('Should get diagnostics', () => {
-	const docUri = getDocUri('Main.jack');
-
-	test('Diagnoses uppercase texts', async () => {
-		await testDiagnostics(docUri, [
-			{ message: "extraneous input 'l' expecting {'constructor', 'function', 'method', 'field', 'static', '}'}", range: toRange(1, 1, 1, 2), severity: vscode.DiagnosticSeverity.Error },
-		
-		]);
-	});
+suite("Should get diagnostics", () => {
+  test("lexer error", async () => {
+    const docUri = getDocUri("LexerError.jack");
+    await testDiagnostics(docUri, [
+      {
+        message: "token recognition error at: '%'",
+        range: toRange(1, 1, 1, 2),
+        severity: vscode.DiagnosticSeverity.Error,
+      },
+    ]);
+  });
+  test("parser error", async () => {
+    const docUri = getDocUri("ParserError.jack");
+    await testDiagnostics(docUri, [
+      {
+        message:
+          "extraneous input 'field' expecting {'int', 'char', 'boolean', IDENTIFIER}",
+        range: toRange(1, 7, 1, 12),
+        severity: vscode.DiagnosticSeverity.Error,
+      },
+    ]);
+  });
+  test("validation error", async () => {
+    const docUri = getDocUri("ClassNotFound.jack");
+    await testDiagnostics(docUri, [
+      {
+        message: "Class B doesn't exist",
+        range: toRange(1, 7, 1, 7),
+        severity: vscode.DiagnosticSeverity.Error,
+      },
+    ]);
+  });
+  test.only("valid", async () => {
+    const docUri = getDocUri("dino/Main.jack");
+    await activate(docUri);
+    const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
+    assert.equal(actualDiagnostics.length, 0);
+  });
 });
 
 function toRange(sLine: number, sChar: number, eLine: number, eChar: number) {
-	const start = new vscode.Position(sLine, sChar);
-	const end = new vscode.Position(eLine, eChar);
-	return new vscode.Range(start, end);
+  const start = new vscode.Position(sLine, sChar);
+  const end = new vscode.Position(eLine, eChar);
+  return new vscode.Range(start, end);
 }
 
-async function testDiagnostics(docUri: vscode.Uri, expectedDiagnostics: vscode.Diagnostic[]) {
-	await activate(docUri);
+async function testDiagnostics(
+  docUri: vscode.Uri,
+  expectedDiagnostics: vscode.Diagnostic[]
+) {
+  await activate(docUri);
 
-	const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
-	console.log("Diagnostics:", actualDiagnostics)
-	assert.equal(actualDiagnostics.length, expectedDiagnostics.length);
+  const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
 
-	expectedDiagnostics.forEach((expectedDiagnostic, i) => {
-		const actualDiagnostic = actualDiagnostics[i];
-		assert.equal(actualDiagnostic.message, expectedDiagnostic.message);
-		assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range);
-		assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity);
-	});
+  assert.equal(actualDiagnostics.length, expectedDiagnostics.length);
+
+  expectedDiagnostics.forEach((expectedDiagnostic, i) => {
+    const actualDiagnostic = actualDiagnostics[i];
+    assert.equal(actualDiagnostic.message, expectedDiagnostic.message);
+    assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range);
+    assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity);
+  });
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,28 @@
 import {
-	createConnection,
-	TextDocuments,
-	Diagnostic,
-	// DiagnosticSeverity,
-	ProposedFeatures,
-	InitializeParams,
-	DidChangeConfigurationNotification,
-	TextDocumentSyncKind,
-	InitializeResult,
-	DocumentDiagnosticReportKind,
-	type DocumentDiagnosticReport,
-	DiagnosticSeverity,
-	DocumentFormattingParams,
-	Position,
-	Range,
-	TextEdit,
-} from 'vscode-languageserver/node';
-import {
-	TextDocument,
-} from 'vscode-languageserver-textdocument';
-import { Compiler, JackCompilerError, ProgramContext } from 'jack-compiler/out/index';
-import * as prettier from 'prettier';
-import { JackPlugin } from 'prettier-plugin-jack/out/index';
-import * as path from 'path';
-import * as fs from 'fs';
-import { URI } from 'vscode-uri';
+  createConnection,
+  TextDocuments,
+  Diagnostic,
+  // DiagnosticSeverity,
+  ProposedFeatures,
+  InitializeParams,
+  DidChangeConfigurationNotification,
+  TextDocumentSyncKind,
+  InitializeResult,
+  DocumentDiagnosticReportKind,
+  type DocumentDiagnosticReport,
+  DiagnosticSeverity,
+  DocumentFormattingParams,
+  Position,
+  Range,
+  TextEdit,
+} from "vscode-languageserver/node";
+import { TextDocument } from "vscode-languageserver-textdocument";
+import { Compiler, JackCompilerError } from "jack-compiler/out/index";
+import * as prettier from "prettier";
+import { JackPlugin } from "prettier-plugin-jack/out/index";
+import * as path from "path";
+import * as fs from "fs";
+import { URI } from "vscode-uri";
 // import { JackCompilerEror } from 'jack-compiler';
 
 // Create a connection for the server, using Node's IPC as a transport.
@@ -42,64 +40,65 @@ const hasFormattingCapability = true;
 let hasDiagnosticRelatedInformationCapability = false;
 
 connection.onInitialize((params: InitializeParams) => {
+  connection.console.log("Server on init...");
+  const capabilities = params.capabilities;
 
-	connection.console.log("Server on init...");
-	const capabilities = params.capabilities;
+  // Does the client support the `workspace/configuration` request?
+  // If not, we fall back using global settings.
+  hasConfigurationCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.configuration
+  );
+  hasWorkspaceFolderCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.workspaceFolders
+  );
+  hasDiagnosticRelatedInformationCapability = !!(
+    capabilities.textDocument &&
+    capabilities.textDocument.publishDiagnostics &&
+    capabilities.textDocument.publishDiagnostics.relatedInformation
+  );
 
-	// Does the client support the `workspace/configuration` request?
-	// If not, we fall back using global settings.
-	hasConfigurationCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.configuration
-	);
-	hasWorkspaceFolderCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.workspaceFolders
-	);
-	hasDiagnosticRelatedInformationCapability = !!(
-		capabilities.textDocument &&
-		capabilities.textDocument.publishDiagnostics &&
-		capabilities.textDocument.publishDiagnostics.relatedInformation
-	);
-
-	const result: InitializeResult = {
-		capabilities: {
-			textDocumentSync: TextDocumentSyncKind.Incremental,
-			// Tell the client that this server supports code completion.
-			completionProvider: {
-				resolveProvider: true
-			},
-			diagnosticProvider: {
-				interFileDependencies: false,
-				workspaceDiagnostics: false
-			},
-			documentFormattingProvider: true,
-		}
-	};
-	if (hasWorkspaceFolderCapability) {
-		result.capabilities.workspace = {
-			workspaceFolders: {
-				supported: true
-			}
-		};
-	}
-	return result;
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+      // Tell the client that this server supports code completion.
+      completionProvider: {
+        resolveProvider: true,
+      },
+      diagnosticProvider: {
+        interFileDependencies: false,
+        workspaceDiagnostics: false,
+      },
+      documentFormattingProvider: true,
+    },
+  };
+  if (hasWorkspaceFolderCapability) {
+    result.capabilities.workspace = {
+      workspaceFolders: {
+        supported: true,
+      },
+    };
+  }
+  return result;
 });
 
 connection.onInitialized(() => {
-
-	if (hasConfigurationCapability) {
-		// Register for all configuration changes.
-		connection.client.register(DidChangeConfigurationNotification.type, undefined);
-	}
-	if (hasWorkspaceFolderCapability) {
-		connection.workspace.onDidChangeWorkspaceFolders(_event => {
-			connection.console.log('Workspace folder change event received.');
-		});
-	}
+  if (hasConfigurationCapability) {
+    // Register for all configuration changes.
+    connection.client.register(
+      DidChangeConfigurationNotification.type,
+      undefined
+    );
+  }
+  if (hasWorkspaceFolderCapability) {
+    connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+      connection.console.log("Workspace folder change event received.");
+    });
+  }
 });
 
 // The example settings
 interface ExampleSettings {
-	maxNumberOfProblems: number;
+  maxNumberOfProblems: number;
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
@@ -112,158 +111,162 @@ let globalSettings: ExampleSettings = defaultSettings;
 // Cache the settings of all open documents
 const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
 
-connection.onDidChangeConfiguration(change => {
-	if (hasConfigurationCapability) {
-		// Reset all cached document settings
-		documentSettings.clear();
-	} else {
-		globalSettings = <ExampleSettings>(
-			(change.settings.languageServerExample || defaultSettings)
-		);
-	}
-	// Refresh the diagnostics since the `maxNumberOfProblems` could have changed.
-	// We could optimize things here and re-fetch the setting first can compare it
-	// to the existing setting, but this is out of scope for this example.
-	connection.languages.diagnostics.refresh();
+connection.onDidChangeConfiguration((change) => {
+  if (hasConfigurationCapability) {
+    // Reset all cached document settings
+    documentSettings.clear();
+  } else {
+    globalSettings = <ExampleSettings>(
+      (change.settings.languageServerExample || defaultSettings)
+    );
+  }
+  // Refresh the diagnostics since the `maxNumberOfProblems` could have changed.
+  // We could optimize things here and re-fetch the setting first can compare it
+  // to the existing setting, but this is out of scope for this example.
+  connection.languages.diagnostics.refresh();
 });
 
 function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
-	if (!hasConfigurationCapability) {
-		return Promise.resolve(globalSettings);
-	}
-	let result = documentSettings.get(resource);
-	if (!result) {
-		result = connection.workspace.getConfiguration({
-			scopeUri: resource,
-			section: 'languageServerExample'
-		});
-		documentSettings.set(resource, result);
-	}
-	return result;
+  if (!hasConfigurationCapability) {
+    return Promise.resolve(globalSettings);
+  }
+  let result = documentSettings.get(resource);
+  if (!result) {
+    result = connection.workspace.getConfiguration({
+      scopeUri: resource,
+      section: "languageServerExample",
+    });
+    documentSettings.set(resource, result);
+  }
+  return result;
 }
 
 // Only keep settings for open documents
-documents.onDidClose(e => {
-	documentSettings.delete(e.document.uri);
+documents.onDidClose((e) => {
+  documentSettings.delete(e.document.uri);
 });
 
-
 connection.languages.diagnostics.on(async (params) => {
-	const document = documents.get(params.textDocument.uri);
-	if (document !== undefined) {
-		return {
-			kind: DocumentDiagnosticReportKind.Full,
-			items: await validateTextDocument(document)
-		} satisfies DocumentDiagnosticReport;
-	} else {
-		// We don't know the document. We can either try to read it from disk
-		// or we don't report problems for it.
-		return {
-			kind: DocumentDiagnosticReportKind.Full,
-			items: []
-		} satisfies DocumentDiagnosticReport;
-	}
+  const document = documents.get(params.textDocument.uri);
+  if (document !== undefined) {
+    return {
+      kind: DocumentDiagnosticReportKind.Full,
+      items: await validateTextDocument(document),
+    } satisfies DocumentDiagnosticReport;
+  } else {
+    // We don't know the document. We can either try to read it from disk
+    // or we don't report problems for it.
+    return {
+      kind: DocumentDiagnosticReportKind.Full,
+      items: [],
+    } satisfies DocumentDiagnosticReport;
+  }
 });
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
-documents.onDidChangeContent(change => {
-	connection.console.log("Server on change...");
-	validateTextDocument(change.document);
+documents.onDidChangeContent((change) => {
+  connection.console.log("Server on change...");
+  validateTextDocument(change.document);
 });
 
-async function validateTextDocument(textDocument: TextDocument): Promise<Diagnostic[]> {
-	console.log(textDocument);
-	// In this simple example we get the settings for every validate run.
-	// const settings = await getDocumentSettings(textDocument.uri);
+async function validateTextDocument(
+  textDocument: TextDocument
+): Promise<Diagnostic[]> {
+  console.log(textDocument);
+  // In this simple example we get the settings for every validate run.
+  // const settings = await getDocumentSettings(textDocument.uri);
 
-	// The validator creates diagnostics for all uppercase words length 2 and more
-	const text = textDocument.getText();
-	const compiler = new Compiler();
-	const selectedFilePath = URI.parse(textDocument.uri).fsPath;
-	const dir = path.dirname(selectedFilePath);
-	// const file = path.resolve(textDocument.uri)
-	connection.console.log("Dir " + dir);
-	connection.console.log("File " + selectedFilePath);
+  // The validator creates diagnostics for all uppercase words length 2 and more
+  const text = textDocument.getText();
+  const compiler = new Compiler();
+  const selectedFilePath = URI.parse(textDocument.uri).fsPath;
+  const dir = path.dirname(selectedFilePath);
+  // const file = path.resolve(textDocument.uri)
+  connection.console.log("Dir " + dir);
+  connection.console.log("File " + selectedFilePath);
 
+  const parsedOrErrors = compiler.parse(text);
+  if (Array.isArray(parsedOrErrors)) {
+    return parsedOrErrors.map((m) => toDiagnostics(textDocument, m));
+  }
 
-	const parsedOrErrors = compiler.parse(text);
-	if (Array.isArray(parsedOrErrors)) {
-		return parsedOrErrors.map(m => toDiagnostics(textDocument, m));
-	}
+  const files = fs
+    .readdirSync(dir)
+    .filter((file) => file.endsWith(".jack"))
+    .map((file) => path.join(dir, file));
+  for (const filePath of files) {
+    if (filePath == selectedFilePath) continue;
+    const content = fs.readFileSync(filePath, { encoding: "utf8", flag: "r" });
+    const treeOrErrors = compiler.parse(content);
+    if (!Array.isArray(treeOrErrors)) {
+      compiler.bind(treeOrErrors);
+    }
+  }
 
-	const files = fs
-		.readdirSync(dir)
-		.filter((file) => file.endsWith(".jack"))
-		.map((file) => path.join(dir, file));
-	for (const filePath of files) {
-		if (filePath == selectedFilePath) continue;
-		const treeOrErrors = compiler.parse(filePath);
-		if (!Array.isArray(treeOrErrors)) {
-			compiler.bind(treeOrErrors);
-		}
-	}
+  const bindedOrErrors = compiler.bind(parsedOrErrors);
+  if (Array.isArray(bindedOrErrors)) {
+    return bindedOrErrors.map((m) => toDiagnostics(textDocument, m));
+  }
 
-	const bindedOrErrors = compiler.bind(parsedOrErrors);
-	if (Array.isArray(bindedOrErrors)) {
-		return bindedOrErrors.map(m => toDiagnostics(textDocument, m));
-	}
-
-	const validatedOrErrors = compiler.validate(bindedOrErrors);
-	if (Array.isArray(validatedOrErrors)) {
-		return validatedOrErrors.map(m => toDiagnostics(textDocument, m));
-	}
-	return [];
+  const validatedOrErrors = compiler.validate(bindedOrErrors);
+  if (Array.isArray(validatedOrErrors)) {
+    return validatedOrErrors.map((m) => toDiagnostics(textDocument, m));
+  }
+  return [];
 }
-
 
 function toDiagnostics(textDocument: TextDocument, e: JackCompilerError) {
-	console.log("Error: " + JSON.stringify(e));
-	return {
-		severity: DiagnosticSeverity.Error,
-		range: {
-			start: textDocument.positionAt(e.span.start),
-			end: textDocument.positionAt(e.span.end),
-		},
-		message: e.msg,
-	};
+  console.log("Error: " + JSON.stringify(e));
+  return {
+    severity: DiagnosticSeverity.Error,
+    range: {
+      start: textDocument.positionAt(e.span.start),
+      end: textDocument.positionAt(e.span.end),
+    },
+    message: e.msg,
+  };
 }
 
-connection.onDidChangeWatchedFiles(_change => {
-	// Monitored files have change in VSCode
-	connection.console.log('We received a file change event');
+connection.onDidChangeWatchedFiles((_change) => {
+  // Monitored files have change in VSCode
+  connection.console.log("We received a file change event");
 });
 
+connection.onDocumentFormatting(
+  async (formatParams: DocumentFormattingParams): Promise<TextEdit[]> => {
+    const document = documents.get(formatParams.textDocument.uri);
 
+    if (!document) {
+      return [];
+    }
 
-connection.onDocumentFormatting(async (formatParams: DocumentFormattingParams): Promise<TextEdit[]> => {
-	const document = documents.get(formatParams.textDocument.uri);
-
-	if (!document) {
-		return [];
-	}
-
-	try {
-		const text = document.getText();
-		const formatted = await prettier.format(text, {
-			parser: "jack",
-			plugins: [JackPlugin],
-			tabWidth: formatParams.options.tabSize,
-			singleQuote: false,
-			useTabs: !formatParams.options.insertSpaces,
-		});
-		connection.console.log("Formatting document: " + formatted);
-		return [TextEdit.replace(Range.create(Position.create(0, 0), document.positionAt(text.length)), formatted)];
-	} catch (error) {
-		if (typeof error == "string")
-			connection.console.error("Error formatting document:" + error);
-		if (error instanceof Error) {
-			connection.console.error("Error formatting document:" + error.message);
-
-		}
-		return [];
-	}
-});
+    try {
+      const text = document.getText();
+      const formatted = await prettier.format(text, {
+        parser: "jack",
+        plugins: [JackPlugin],
+        tabWidth: formatParams.options.tabSize,
+        singleQuote: false,
+        useTabs: !formatParams.options.insertSpaces,
+      });
+      connection.console.log("Formatting document: " + formatted);
+      return [
+        TextEdit.replace(
+          Range.create(Position.create(0, 0), document.positionAt(text.length)),
+          formatted
+        ),
+      ];
+    } catch (error) {
+      if (typeof error == "string")
+        connection.console.error("Error formatting document:" + error);
+      if (error instanceof Error) {
+        connection.console.error("Error formatting document:" + error.message);
+      }
+      return [];
+    }
+  }
+);
 // console.log = connection.console.log;
 
 // Make the text document manager listen on the connection
